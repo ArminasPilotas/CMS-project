690fe03982c1225b518f598e2a82316d94f4a2df
Armin <arminas.pilotas@gmail.com>
2020-04-19 11:12:35 +0300

    Updated password encryption

--- admin/profile.php
+++ admin/profile.php
@@ -16,7 +16,6 @@ while($row = mysqli_fetch_array($select_user_profile_query)){
     $user_lastname =$row['user_lastname'];
     $user_email =$row['user_email'];
     $user_image =$row['user_image'];
-    $user_role =$row['user_role'];
 }
 }
 ?>
@@ -25,7 +24,6 @@ while($row = mysqli_fetch_array($select_user_profile_query)){
 if(isset($_POST['edit_user'])){
     $user_firstname= $_POST['user_firstname'];
     $user_lastname= $_POST['user_lastname'];
-    $user_role= $_POST['user_role'];
     /*$post_image= $_FILES['image']['name'];
     $post_image_temp= $_FILES['image']['tmp_name'];*/
     $username= $_POST['username'];
@@ -39,7 +37,6 @@ if(isset($_POST['edit_user'])){
     $query= "UPDATE users SET ";
     $query .="user_firstname ='{$user_firstname}', ";
     $query .="user_lastname ='{$user_lastname}', ";
-    $query .="user_role ='{$user_role}', ";
     $query .="username ='{$username}', ";
     $query .="user_email ='{$user_email}', ";
     $query .="user_password ='{$user_password}' ";
@@ -84,22 +81,6 @@ if(isset($_POST['edit_user'])){
                         </div>
 
 
-                        <div class="form-group">
-                            <select name="user_role" id="">
-                                <option value="Subscriber"><?php echo $user_role; ?></option>
-                                <?php
-                                if($user_role == 'Admin'){
-                                    echo  "<option value='Subscriber'>Subscriber</option>";
-                                }
-                                else {
-                                    echo  "<option value='Admin'>Admin</option>";
-                                }
-                                ?>
-
-
-                            </select>
-                        </div>
-
                         <!--<div class="form-group">
                         <label for="post_image">Post Image</label>
                         <input type="file" name="image">
@@ -117,7 +98,7 @@ if(isset($_POST['edit_user'])){
 
                         <div class="form-group">
                             <label for="post_content">Password </label>
-                            <input type="password" value="<?php echo $user_password; ?>" class="form-control" name="user_password">
+                            <input autocomplete="off" type="password" class="form-control" name="user_password">
                         </div>
 
                         <div class="form-group">
